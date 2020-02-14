f1b0fe8411d1b55200a0e60d44a28a6d41e36326
Luis Mayta <luis@hadenlabs.com>
2020-02-14 18:14:21 -0500

    refactor: settings project (#16)

---
+++ {{cookiecutter.repository_name}}/.eslintrc.js
@@ -0,0 +1,45 @@
+const prettierrc = require('@equipindustry/prettierrc');
+module.exports = {
+  parser: 'babel-eslint',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    project: './tsconfig.json',
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  rules: {
+    'prettier/prettier': [
+      'error',
+      prettierrc,
+      {
+        usePrettierrc: true,
+      },
+    ],
+  },
+  overrides: [
+    {
+      files: ['**/*.ts', '**/*.tsx'],
+      env: { browser: true, es6: true, node: true },
+      extends: ['@equipindustry/eslint-config'],
+      parser: '@typescript-eslint/parser',
+      parserOptions: {
+        ecmaFeatures: { jsx: true },
+        ecmaVersion: 2018,
+        sourceType: 'module',
+        project: './tsconfig.json',
+      },
+      plugins: ['prettier', '@typescript-eslint'],
+      rules: {
+        'prettier/prettier': [
+          'error',
+          prettierrc,
+          {
+            usePrettierrc: true,
+          },
+        ],
+      },
+    },
+  ],
+};
